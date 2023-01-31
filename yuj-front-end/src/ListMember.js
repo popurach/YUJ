96c0ce24ecd183d00c728f167b6bab81ad01e0c8
이호준 <2hojune@naver.com>
2023-01-31 13:19:14 +0900

    "open-vidu 화상 및 채팅방, 세션 목록 확인기능 구현 완료"
    open-vidu 화상 및 채팅방, 세션 목록 확인 기능 구현

---
+++ yuj-front-end/src/ListMember.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Member = styled.p`
+    position: relative !important;
+    display: flex;
+    font-size: 1rem;
+    span{
+        padding-left: 30px;
+    }
+`;
+
+const ListMember = ({ listMember }) => {
+    return (
+        <Member>
+            <>{listMember[0]}</>
+            {
+                listMember[1] === true ?
+                <span class="material-symbols-outlined">videocam</span> : <span class="material-symbols-outlined">  videocam_off</span>
+            }
+            {
+                listMember[2] === true ?
+                <span class="material-symbols-outlined">  mic</span> : <span class="material-symbols-outlined">  mic_off</span>
+            }
+        </Member>
+    )
+}
+
+export default ListMember;
---
+++ yuj-front-end/src/ListMembers.js
@@ -0,0 +1,44 @@
+import React, { useState , memo , useEffect , useRef, createRef} from 'react';
+import styled from 'styled-components';
+import './index.css';
+import ListMember from './ListMember' 
+
+const MemberCount = styled.p`
+    position: relative !important;
+    font-size: 1rem;
+    font-weight: bold;
+    color: white;
+`;
+
+const MemberBox = styled.div`
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.33);
+    border-radius: 20px;
+    width: 500px;
+    height: 310px;
+
+    overflow-y: scroll;
+    word-break: break-all;
+    line-break: normal;
+    position: absolute;
+    bottom: 60%;
+    right:0;
+`;
+const ListMembers = ({ listMembers }) => {
+    return (
+        <>
+            <MemberBox>
+                <MemberCount>참가자({ listMembers.length})</MemberCount>
+                {
+                    listMembers?.map((listMember, i) => (
+                        <div key={'list member' + listMember[0] + i}>
+                            <ListMember listMember={ listMember } />
+                        </div>
+                    ))
+                }
+            </MemberBox>
+        </>
+    )
+}
+
+export default ListMembers;
