9a8056047b8c6b07f9c8daa8bf122b2ea9f484e7
ohhyunkyu <uyk0928@gmail.com>
2023-01-31 17:42:23 +0900

    헤더 및 푸터 제작 중

---
+++ yuj-front-end/src/components/mainFooter/MainFooter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainFooter = () => {
+    return (
+        <footer className="footer footer-center p-4 bg-secondary text-base-content" style={{height:'25px', padding:0}}>
+            <div>
+                <p style={{fontSize:'11px'}}>Copyright © 2023 - All right reserved by Yuj.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default MainFooter
