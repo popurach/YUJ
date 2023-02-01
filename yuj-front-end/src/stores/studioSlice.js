07e7e5a49bd36b0fe2a402d9b650131e631c8cb0
김인중 <qwe_asd13579@naver.com>
2023-02-01 17:46:02 +0900

    Refactor: "샘플코드 주석 추가"
    샘플코드 설명 주석 추가 & 스타일 변경
    css 주석 에러 수정

--- yuj-front-end/src/stores/studioSlice.js
+++ yuj-front-end/src/stores/studioSlice.js
@@ -9,24 +9,36 @@ import axios from 'axios';
 /**
  * 2. Axios 함수 만들기(API를 호출하는 함수)
  */
+
+// 특정 스튜디오의 정보를 가져오는 함수
 const getStudio = createAsyncThunk("GET_STUDIO", async(studioId) => {
+
+	// Studio의 정보를 GET 메소드로 가져오는 api 호출
 	const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${studioId}`);
+
+	// 가져온 Studio 데이터를 리턴
 	return response.data;
 })
+
+// 새로운 스튜디오를 등록하는 함수
 const createStudio = createAsyncThunk("CREATE_STUDIO", async(studio) => {
+
+	// post로 넘겨줄 스튜디오 form 객체 만들기
 	const studioForm = {
 		'userId': studio.teacherId,
 		'title': studio.studioName,
 		'body': studio.studioDesc
 	}
-
 	console.log(studioForm);
 
+	// 만들어준 스튜디오 form 객체를 post로 보내는 api 호출
 	const response = await axios.post(`https://jsonplaceholder.typicode.com/posts`,JSON.stringify(studioForm),{
 		headers: {
 			'Content-type': 'application/json; charset=UTF-8',
 		}
 	});
+
+	// 저장 성공 여부 데이터를 리턴
 	return response.data;
 })
 
@@ -44,16 +56,23 @@ const studioSlice = createSlice({
 		teacherId: 0,
 		studioName: "초기값",
 		studioDesc: "초기값",
+		studioList: [],
 	},
 
 	// reducers => State를 바꿀 수 있는 모든 함수(= action) 정의
 	reducers: {
 		changeStudioName:(state, action) => {
-			state.studioName = action.payload;
+			const newStudioName = action.payload;
+			state.studioName = newStudioName;
 		},
 		changeStudioDesc:(state, action) => {
-			state.studioDesc = action.payload;
+			const newStudioDesc = action.payload;
+			state.studioDesc = newStudioDesc;
 		},
+		addStudioToList:(state, action) => {
+			const newStudio = action.payload;
+			state.studioList = [...state.studioList, newStudio];
+		}
 	},
 
 	// extraReducers => Axios 함수의 상태(Success, Failed, Pending)에 따른 동작 정의
@@ -80,7 +99,7 @@ const studioSlice = createSlice({
 export default studioSlice;
 
 // 일반 action export 하기
-export const { changeStudioName, changeStudioDesc } = studioSlice.actions;
+export const { changeStudioName, changeStudioDesc, addStudioToList } = studioSlice.actions;
 
 // Axios 비동기 action export 하기
 export { getStudio, createStudio };
