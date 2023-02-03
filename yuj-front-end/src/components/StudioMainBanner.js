816de64e5040688b69226d77ad5cec61bcbbd50c
김인중 <qwe_asd13579@naver.com>
2023-02-03 13:03:23 +0900

    Feat: "스튜디오 메인페이지 배너 컴포넌트 제작"
    스튜디오 메인 페이지 베너 컴포넌트 제작

---
+++ yuj-front-end/src/components/StudioMainBanner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const StudioMainBanner = () => {
+    return (
+        <>
+        <div className="flex">
+            <div className={'w-full flex h-80 overflow-hidden items-center justify-center'}>
+                <img className={''} src="./assets/Sample3.jpg" />
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default StudioMainBanner;
