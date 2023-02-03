cab250e8030e4abdb5cdfa822a6ecb341dd9eee0
ohhyunkyu <uyk0928@gmail.com>
2023-02-03 13:18:47 +0900

    Feat: 강의 상세 페이지 구성 중

--- yuj-front-end/src/components/lectureDetail/lectureDetailSchedule/LectureDetailSchedule.js
+++ yuj-front-end/src/components/lectureDetail/lectureDetailSchedule/LectureDetailSchedule.js
@@ -3,9 +3,9 @@ import LectureDetailScheduleCard from './LectureDetailScheduleCard';
 
 const LectureDetailSchedule = () => {
     return (
-        <div className='my-7'>
-            <p className='text-lg font-bold text-accent mb-6'>수업일정</p>
-            <div className='flex gap-3'>
+        <div className='my-7' style={{width:'50rem'}}>
+            <p className='text-lg font-bold text-accent mb-5'>수업일정</p>
+            <div className='flex gap-3 flex-wrap justify-start'>
                 <LectureDetailScheduleCard />
                 <LectureDetailScheduleCard />
                 <LectureDetailScheduleCard />
