bdf6ae60c77be780b4b6770434560db7aa537f81
ohhyunkyu <uyk0928@gmail.com>
2023-02-06 17:42:41 +0900

    Feat: commonSlice 추가 & 강사 강의 생성 페이지에 필요한 컴포넌트 작성 및 화면 구성

---
+++ yuj-front-end/src/components/StudioLectureCreateImagesInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudioLectureCreateImagesInput = () => {
+  return (
+    <div>StudioLectureCreateImagesInput</div>
+  )
+}
+
+export default StudioLectureCreateImagesInput
---
+++ yuj-front-end/src/components/StudioLectureCreateScheduleInput.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
+const StudioLectureCreateScheduleInput = () => {
+
+    const schedule =
+        <div className='flex gap-3 items-center'>
+            <select class="select flex-auto max-w-xs bg-primary">
+                <option disabled selected>요일</option>
+                <option>Homer</option>
+                <option>Marge</option>
+                <option>Bart</option>
+                <option>Lisa</option>
+                <option>Maggie</option>
+            </select>
+            <select class="select flex-auto max-w-xs bg-primary">
+                <option disabled selected>시작</option>
+                <option>Homer</option>
+                <option>Marge</option>
+                <option>Bart</option>
+                <option>Lisa</option>
+                <option>Maggie</option>
+            </select>
+            <select class="select flex-auto max-w-xs bg-primary">
+                <option disabled selected>종료</option>
+                <option>Homer</option>
+                <option>Marge</option>
+                <option>Bart</option>
+                <option>Lisa</option>
+                <option>Maggie</option>
+            </select>
+            <DeleteForeverIcon className='text-accent' style={{fontSize:"xx-large"}}/>
+        </div>
+
+
+    return (
+        <>
+            <div className='flex items-center mb-4'>
+                <p className='text-lg text-accent'>수업 일정</p>
+                <AddCircleIcon className='ml-2 text-accent' />
+            </div>
+            {schedule}
+        </>
+    )
+}
+
+export default StudioLectureCreateScheduleInput
