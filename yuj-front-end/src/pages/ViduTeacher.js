297ce85b304dafaf48339a897c123320ab903892
김인중 <qwe_asd13579@naver.com>
2023-02-06 16:44:15 +0900

    Refactor: "오픈비두 접속 url 수정"

--- yuj-front-end/src/pages/ViduTeacher.js
+++ yuj-front-end/src/pages/ViduTeacher.js
@@ -10,7 +10,7 @@ import { Base64 } from 'js-base64';
 import { SignalCellularNull } from "@mui/icons-material";
 
 const APPLICATION_SERVER_URL = "https://i8a504.p.ssafy.io/";
-const OPENVIDU_SERVER_URL = 'https://i8a504.p.ssafy.io/openvidu';
+const OPENVIDU_SERVER_URL = 'https://i8a504.p.ssafy.io';
 // const APPLICATION_SERVER_URL = "http://localhost:5000/";
 // const OPENVIDU_SERVER_URL = 'http://localhost:4443';
 const OPENVIDU_SERVER_SECRET = '123123';
