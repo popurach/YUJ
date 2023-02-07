7a01163386f6e26833e15c35e62a33a91325c0fa
oyo87 <ldeathnote@naver.com>
2023-02-08 01:35:53 +0900

    Feat: "마이페이지 대시보드 현재수강중, 수강완료강의 임시" 이미지, axios임시데이터 이용해서 데이터 가져와서 화면을 구성해보았습니다.

--- yuj-front-end/src/pages/MyPageDashBoard.js
+++ yuj-front-end/src/pages/MyPageDashBoard.js
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MyPageSidebar from "../components/MyPageSidebar";
 import Styles from "./MyPages.module.css";
 import MainHeader from './../components/mainHeader/MainHeader';
 import MainFooter from "../components/mainFooter/MainFooter";
 import MyPageWeeklyStudyChart from '../components/MyPageWeeklyStudyChart';
 import MyPageCalendar from "../components/MyPageCalendar";
+import axios from "axios";
+
+//현재 수강중인 강의 썸네일+ 제목, 다음수업 일정 더미
+
+//수강 완료 강의 썸네일 + 제목, 완료 수강일 더미
+function CompletedLecture() {
+    return (
+        <div className="h-20 my-2 flex">
+            <div className="h-full w-32 mx-5">
+                <img src="/assets/Sample2.jpg"></img>
+            </div>
+            <div className="leading-loose">요가의 완성
+                <div>완료 수강일 : 2023-02-08</div>
+            </div>
+        </div>
+    )
+}
 
 const MyPageDashBoard = () => {
+
+    //현재 수강중인 강의
+    const [currentLectures, setCurrentLectures] = useState([]);
+    //수강 완료한 강의
+    const [CompletedLectures, setCompletedLectures] = useState([]);
+
+    useEffect(() => {
+        //현재 수강중인 강의 가져와야하는부분 현재 임시데이터
+        axios({
+            method: "GET",
+            url: "https://jsonplaceholder.typicode.com/posts"
+        }).then(response => setCurrentLectures(response.data))
+            .catch(error => {
+                console.log(error.response);
+            })
+
+        //수강 완료한 강의 가져와야하는부분 현재 임시데이터
+        axios({
+            method: "GET",
+            url: "https://jsonplaceholder.typicode.com/posts"
+        }).then(response => setCompletedLectures(response.data))
+            .catch(error => {
+                console.log(error.response);
+            })
+    })
+
+
     return (
         <>
             <MainHeader />
             <container className="flex ml-100">
                 <MyPageSidebar />
                 <main>
-                    <div className="mx-28 mt-16">
+                    <div className="mx-28 mt-16 w-full">
                         <div className="text-3xl font-bold">마이 페이지 - 대시보드</div>
                         <div className="w-full flex">
                             <div className={Styles[`dashboard-box`]}>
@@ -25,13 +69,23 @@ const MyPageDashBoard = () => {
                                     </div>
                                     <div>전체보기 &gt;</div>
                                 </div>
-                                <div>
-                                </div>
-
-                                만약 1개도 존재하지 않으면 수강중인 강의가 없습니다.
+                                {/* 만약 1개도 존재하지 않으면 수강중인 강의가 없습니다.
                                 get으로 강의리스트 가져오고 최신3개까지만 썸네일 가져와서
                                 좌측div에 강의썸네일 우측에는 강의제목, 강의예정 시간
-                                url링크 걸어서 강의 스튜디오로이동해야함
+                                url링크 걸어서 강의 스튜디오로이동해야함 */}
+                                {currentLectures.slice(0, 3).map(post => (
+                                    <>
+                                        <div className="h-20 my-2 flex">
+                                            <div className="h-full w-32 mx-5">
+                                                <img src="/assets/Sample.jpg"></img>
+                                            </div>
+                                            <div className="leading-loose">{post.id}
+                                                <div>수업 예정 : {post.id}</div>
+                                            </div>
+                                        </div>
+                                    </>
+                                ))}
+
 
                             </div>
                             <div className={Styles[`dashboard-box`]}>
@@ -44,13 +98,23 @@ const MyPageDashBoard = () => {
                                     <div>전체보기 &gt;</div>
                                 </div>
                                 <div>
-
+                                    {currentLectures.slice(0, 3).map(post => (
+                                        <>
+                                            <div className="h-20 my-2 flex">
+                                                <div className="h-full w-32 mx-5">
+                                                    <img src="/assets/Sample2.jpg"></img>
+                                                </div>
+                                                <div className="leading-loose">{post.id}
+                                                    <div>완료 수강일 : {post.id}</div>
+                                                </div>
+                                            </div>
+                                        </>
+                                    ))}
                                 </div>
-
-                                만약 1개도 존재하지 않으면 수강완료한 강의가 없습니다.
+                                {/* 만약 1개도 존재하지 않으면 수강완료한 강의가 없습니다.
                                 get으로 강의리스트 가져오고 최신 완료한 3개까지만 썸네일 가져와서
                                 좌측div에 강의썸네일 우측에는 강의제목, 강의완료 날짜
-                                url링크 걸어서 강의 스튜디오로이동해야함
+                                url링크 걸어서 강의 스튜디오로이동해야함 */}
                             </div>
                             <div className={Styles[`dashboard-box`]}>
                                 <div className={"pl-5 pt-5 " + Styles[`box-font`]}>주간 학습 달성률</div>
