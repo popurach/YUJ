1212887baa969adef0d5dc2f108cf82a785efc76
김인중 <qwe_asd13579@naver.com>
2023-02-07 18:00:10 +0900

    Feat: "Studio 조회 API"
    강사의 UserId를 받아 Studio 정보를 조회하는 API

--- yuj-back-end/src/main/java/com/yuj/studio/controller/StudioController.java
+++ yuj-back-end/src/main/java/com/yuj/studio/controller/StudioController.java
@@ -1,4 +1,31 @@
 package com.yuj.studio.controller;
 
+import com.yuj.studio.dto.response.StudioResponseDTO;
+import com.yuj.studio.service.StudioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/studio")
 public class StudioController {
+
+    private final StudioService studioService;
+
+    @Autowired
+    public StudioController(StudioService studioService) {
+        this.studioService = studioService;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<StudioResponseDTO> getStudio(@PathVariable Long userId) throws Exception {
+        StudioResponseDTO studioResponseDTO = studioService.getStudioByUserId(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(studioResponseDTO);
+    }
 }
--- yuj-back-end/src/main/java/com/yuj/studio/dto/response/StudioResponseDTO.java
+++ yuj-back-end/src/main/java/com/yuj/studio/dto/response/StudioResponseDTO.java
@@ -1,4 +1,13 @@
 package com.yuj.studio.dto.response;
 
+import com.yuj.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class StudioResponseDTO {
+    private Long studioId;
+    private String bannerImage;
+    private String description;
 }
