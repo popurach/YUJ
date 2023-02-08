b673319bd0a27f8ab6df2c9029daebf11386218b
김인중 <qwe_asd13579@naver.com>
2023-02-08 18:09:39 +0900

    Feat: "강의 조회 api"
    강의 id로 강의 조회하기, 강사 id로 개설한 강의목록 조회하기 API 제작

--- yuj-back-end/src/main/java/com/yuj/lecture/dto/response/LectureResponseDTO.java
+++ yuj-back-end/src/main/java/com/yuj/lecture/dto/response/LectureResponseDTO.java
@@ -1,4 +1,26 @@
 package com.yuj.lecture.dto.response;
 
+import com.yuj.lecture.domain.Yoga;
+import com.yuj.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
 public class LectureResponseDTO {
+    private Long lectureId;
+    private String name;
+    private String description;
+    private String thumbnailImage;
+    private LocalDate registDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int limitStudents;
+    private int fee;
+    private int totalCount;
+    private User user;
+    private Yoga yoga;
 }
