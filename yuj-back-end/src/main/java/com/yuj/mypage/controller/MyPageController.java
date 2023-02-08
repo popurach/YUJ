af66a5a2986f48f62d2a2ec56565bdd3bcd40905
배진호 <qowlsgh99@naver.com>
2023-02-08 17:53:37 +0900

    Feat: "마이페이지 백코드 구성중, DB 로컬으로 경로 변경해둔상태"
    원격 DB는 주석처리해두고 로컬을 연결해둔 상태입니다.

--- yuj-back-end/src/main/java/com/yuj/mypage/controller/MyPageController.java
+++ yuj-back-end/src/main/java/com/yuj/mypage/controller/MyPageController.java
@@ -1,38 +1,49 @@
 package com.yuj.mypage.controller;
 
+import com.yuj.lecture.domain.UserLecture;
 import com.yuj.mypage.dto.request.MyPageRequestDTO;
 import com.yuj.mypage.dto.response.MyPageResponseDTO;
 import com.yuj.mypage.service.MyPageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/mypage")
+@RequiredArgsConstructor
 public class MyPageController {
 
-    @Autowired
     private final MyPageService myPageService;
 
+    @GetMapping("/dashboard/{id}")
+    public ResponseEntity<?> getUserLecture(@PathVariable String id) {
+        System.out.println(id);
+        System.out.println(1);
+        List<UserLecture> userLecturesById = myPageService.getUserLecturesById(id);
+        //오늘날짜이후로 가장 가까운 start시간 3개로해야함 일단은 전체 lecture라도 반환해보기
+        System.out.println(userLecturesById);
+
+        return ResponseEntity.ok().body(userLecturesById);
+//        return new ResponseEntity(userLecturesById, HttpStatus.OK);
+    }
 
 
-//    @Autowired
-//    public MyPageController(MyPageService myPageService) {
-//        this.myPageService = myPageService;
-//    }
-//    @GetMapping("/info/1")
-//    public ResponseEntity<?> Test(){
-//
-//        MyPageResponseDTO dto = new MyPageResponseDTO(1L,"2","3","4","5","6");
-//
-//        return new ResponseEntity(dto, HttpStatus.OK);
-//    }
+    @GetMapping("/info/1")
+    public ResponseEntity<?> Test1() {
 
-//    @GetMapping("/test")
-//    public ResponseEntity<?> Test() {
-//        return new ResponseEntity<>("succedss", HttpStatus.BAD_GATEWAY);
-//    }
+        MyPageResponseDTO dto = new MyPageResponseDTO(1L, "2", "3", "4", "5", "6");
+
+        return new ResponseEntity(dto, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> Test() {
+        return new ResponseEntity<>("success", HttpStatus.BAD_GATEWAY);
+    }
 
 //    @PutMapping("/info/{id}")
 //    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody MyPageRequestDTO myPageRequestDTO) {
