4e1537da09ed4869618ee44aabf35081ca0e4605
김인중 <qwe_asd13579@naver.com>
2023-02-09 17:27:33 +0900

    Feat: "스튜디오 슬라이스 수정 & 유저 슬라이스 추가"

--- yuj-front-end/src/stores/index.js
+++ yuj-front-end/src/stores/index.js
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import studioSampleSlice from './studioSampleSlice'
-import studioSlice from './studioSlice'
+import studioSampleSlice from './studioSampleSlice';
+import studioSlice from './studioSlice';
 import commonSlice from './commonSlice';
+import userSlice from './userSlice';
 
 const store = configureStore({
     reducer: {
         common: commonSlice.reducer,
         studioSample: studioSampleSlice.reducer,
         studio: studioSlice.reducer,
+        user: userSlice.reducer,
     }
 })
 
