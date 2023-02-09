b1990f2fc28a83e2ea7477db4d3ea6e8e765245e
ohhyunkyu <uyk0928@gmail.com>
2023-02-09 14:45:11 +0900

    요가 카테고리 관련 백 API & 프론트엔드 렌더링 연결 작업

--- yuj-front-end/src/components/StudioLectureListCategorySelectBox.js
+++ yuj-front-end/src/components/StudioLectureListCategorySelectBox.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { getYogaList } from "../stores/commonSlice";
 
 
 const StudioLectureListCategorySelectBox = () => {
+  
+  //컴포넌트가 마운트 될때 yoga category를 데이터베이스에서 불러와 셀렉트 박스에 띄우기
+  const distpatch = useDispatch();
+  //아래의 빈 [] 배열을 넣어주어야 화면이 첫 렌더링 될 때 한번만 실행됨.
+  useEffect(() => {
+    console.log('mount');
+    distpatch(getYogaList());
+    return () =>{
+    };
+  }, [])
+
   const yogaCategory = useSelector(state => state.common.yogaCategory)
   
   const [selected, setSelected] = useState("default");
@@ -22,8 +34,8 @@ const StudioLectureListCategorySelectBox = () => {
           Select Category
         </option>
         {yogaCategory.map((category) => (
-          <option value={category} key={category}>
-            {category}
+          <option value={category.yogaId} key={category.yogaId}>
+            {category.name + "(" + category.englishName + ")"}
           </option>
         ))}
       </select>
