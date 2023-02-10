0eb3b31846fa602f2d64d6b7c769590e06274d14
김인중 <qwe_asd13579@naver.com>
2023-02-10 17:02:55 +0900

    Feat: "로그인 API 연동"

--- yuj-front-end/src/stores/userSlice.js
+++ yuj-front-end/src/stores/userSlice.js
@@ -1,26 +1,59 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import { useNavigate } from 'react-router';
 import axios from 'axios';
+
+const loginRequest = createAsyncThunk("LOGIN", async(userId, password) => {
+	const loginForm = {
+		id: userId,
+		password: password
+	}
+
+	const response = await axios.post(`https://i8a504.p.ssafy.io/api/login`,JSON.stringify(loginForm));
+	console.log("LOGIN: ",response);
+
+	return response.data;
+})
+
+const decodeJwtToken = (token) => {
+	const base64Payload = token.split('.')[1];
+	const payload = Buffer.from(base64Payload, 'base64'); 
+	const result = JSON.parse(payload.toString());
+	console.log(result);
+	return result.sub;
+}
 
 const userSlice = createSlice({
 	name:'userSlice',
 
 	initialState:{
-		userId: 0,
-		username: "김인중",
-		nickname: "요가소년",
-		token: "yuj1234"
+		tokenInfo: {
+			grantType: "",
+			accessToken: "",
+			refreshToken: "",
+			accessTokenExpireDate: 0
+		},
+		userId: ''
 	},
 
 	reducers: {
-		changeToken:(state, action) => {
-			const newToken = action.payload;
-			state.token = newToken;
+		clearUserState:(state, action) => {
+			state.tokenInfo = {};
+			state.userInfo = '';
 		}
 	},
 
-	extraReducers: {}
+	extraReducers: {
+		[loginRequest.fulfilled]: (state, {payload}) => {
+			const tokenInfo = payload.data;
+			state.tokenInfo = tokenInfo;
+			state.userId = decodeJwtToken(tokenInfo.accessToken);
+			useNavigate('/');
+		},
+	}
 });
 
 export default userSlice;
 
-export const { changeToken } = userSlice.actions;
+export const { clearUserState } = userSlice.actions;
+
+export { loginRequest };
