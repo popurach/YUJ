bf6887bcd3f62969ee4b3c73b4341f72670ba0ed
Edward <qwe_asd13579@naver.com>
2023-02-11 15:07:33 +0900

    Feat: "새로고침 시 Redux의 state 유지"
    Redux persist 라이브러리를 사용해 새로고침 시에도 Redux의 상태가 그대로 유지될 수 있도록 함

--- yuj-front-end/src/stores/index.js
+++ yuj-front-end/src/stores/index.js
@@ -1,18 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
 import studioSampleSlice from './studioSampleSlice';
 import studioSlice from './studioSlice';
 import commonSlice from './commonSlice';
 import lectureSlice from './lectureSlice';
 import userSlice from './userSlice';
 
+const reducers = combineReducers({
+    common: commonSlice.reducer,
+    studioSample: studioSampleSlice.reducer,
+    studio: studioSlice.reducer,
+    lecture: lectureSlice.reducer,
+    user: userSlice.reducer,
+})
+
+const persistConfig = {
+    key: 'yuj',
+    storage,
+    blacklist: ['studio', 'lecture', 'studioSample'],
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-    reducer: {
-        common: commonSlice.reducer,
-        studioSample: studioSampleSlice.reducer,
-        studio: studioSlice.reducer,
-        lecture: lectureSlice.reducer,
-        user: userSlice.reducer,
-    }
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: false
+    })
 })
 
 export default store;
