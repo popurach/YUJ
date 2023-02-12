2388dc8c0b16d43dd96e955ed702830689b969da
uyk1 <uyk0928@gmail.com>
2023-02-12 21:33:58 +0900

    Fix: 강의 상세에서 강의 일정에 관한 오류로 화면이 뜨지 않는 현상 해결

--- yuj-front-end/src/components/StudioLectureDetailSchedule.js
+++ yuj-front-end/src/components/StudioLectureDetailSchedule.js
@@ -4,17 +4,17 @@ import LectureDetailScheduleCard from "./StudioLectureDetailScheduleCard";
 const StudioLectureDetailSchedule = (props) => {
   const lectureSchedule = props.lectureSchedule;
 
-  const schedules = lectureSchedule.map((schedule) => (
-    <LectureDetailScheduleCard
-      schedule={schedule}
-      key={schedule.scheduleId}
-    />
-  ));
-
   return (
     <div className="my-7">
       <p className="text-lg font-bold text-accent mb-5">수업일정</p>
-      <div className="flex gap-3 flex-wrap justify-start">{schedules}</div>
+      <div className="flex gap-3 flex-wrap justify-start">
+        {lectureSchedule.map((schedule) => (
+          <LectureDetailScheduleCard
+            schedule={schedule}
+            key={schedule.scheduleId}
+          />
+        ))}
+      </div>
     </div>
   );
 };
