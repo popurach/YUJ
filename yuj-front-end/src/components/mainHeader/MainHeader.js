18398e282de6fda9fdff19ec28bcbe4796cacaf0
Edward <qwe_asd13579@naver.com>
2023-02-12 17:56:46 +0900

    Refactor: "메인헤더 검색기능 활성화"

--- yuj-front-end/src/components/mainHeader/MainHeader.js
+++ yuj-front-end/src/components/mainHeader/MainHeader.js
@@ -1,10 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { searchLectures } from '../../stores/lectureSlice';
+import { searchTeachers } from '../../stores/studioSlice';
 import styles from './MainHeader.module.css'
 import SearchIcon from '@mui/icons-material/Search';
 import LoginBox from './LoginBox';
+import { useDispatch } from 'react-redux';
 
 const MainHeader = () => {
+
+  const [keyword, setKeyword] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const doSearch = () => {
+    navigate('/search',{state:{keyword:keyword}});
+    dispatch(searchLectures(keyword));
+    dispatch(searchTeachers(keyword));
+  }
+
+
+  const handleOnKeyPress = (e) => {
+    console.log(e);
+    if (e.key === 'Enter') {
+      doSearch();
+    }
+  }
+
   return (
     <div className="navbar bg-secondary" style={{ height: '100px', minHeight: '100px' }}>
       <div className="flex-1">
@@ -14,8 +36,10 @@ const MainHeader = () => {
       </div>
       <div className="flex-none" >
         <div className="form-control" style={{ position: 'relative', paddingRight: '15px' }}>
-          <SearchIcon style={{ position: 'absolute', top:'8px', right:'20px', height:'0.75rem'}}/>
-          <input type="text" placeholder="#강의#강사" className={styles.search + " input input-bordered"} />
+          <input onKeyPress={handleOnKeyPress} onChange={(e) => setKeyword(e.target.value)} type="text" placeholder="#강의#강사" className={styles.search + " input input-bordered"} />
+          <button onClick={() => doSearch()} className={'btn btn-xs btn-ghost border-none btn-circle'}  style={{ position: 'absolute', top:'2px', right:'20px', height:'1rem'}}>
+            <SearchIcon style={{height:'1rem'}}/>
+          </button>
         </div>
         <ul className="menu menu-horizontal px-1 items-center">
           <li className='px-2'><Link to="/" className={styles.menu}>메인 페이지</Link></li>
