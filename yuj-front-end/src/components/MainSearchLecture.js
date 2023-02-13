6d042a932d898ba631a3c5a43a94ae44e31ac397
이호준 <2hojune@naver.com>
2023-02-13 17:36:37 +0900

    Feat : "강의, 강사 전체 검색 구현"
    강의, 강사 전체 검색 구현

--- yuj-front-end/src/components/MainSearchLecture.js
+++ yuj-front-end/src/components/MainSearchLecture.js
@@ -1,85 +1,53 @@
-import { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect,useState } from "react";
+import { useSelector, useDispatch} from 'react-redux';
+import LectureItemCard from "./LectureItemCard";
 import { searchLectures } from '../stores/lectureSlice';
-import { searchTeachers } from '../stores/studioSlice';
-import { useNavigate, useLocation } from 'react-router-dom'
 import SearchIcon from '@mui/icons-material/Search';
-import SearchTeacherListItem from '../components/SearchTeacherListItem'
-import ListTitle from '../components/ListTitle'
-import LectureItemCard from '../components/LectureItemCard';
+import StudioLectureListCategorySelectBox from "./StudioLectureListCategorySelectBox";
 
-const MainSearchPage = (props) => {
+const MainSearchTeacher = () => {
+    const [keyword, setKeyword] = useState('');
+    let lectures = useSelector(state => state.lecture.lecturesSearched)
+    const dispatch = useDispatch();
 
-  const [keyword, setKeyword] = useState('');
-
-  const lecture = useSelector(state => state.lecture);
-  const studio = useSelector(state => state.studio);
-
-  const dispatch = useDispatch();
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const doSearch = () => {
-    dispatch(searchLectures(keyword));
-    dispatch(searchTeachers(keyword));
-  }
-
-  const handleOnKeyPress = (e) => {
-    console.log(e);
-    if (e.key === 'Enter') {
-      doSearch();
+    const doSearch = () => {
+        dispatch(searchLectures(keyword));
     }
-  }
-
-  useEffect(() => {
-    if(location.state != null){
-      setKeyword(location.state.keyword);
+    
+    const handleOnKeyPress = (e) => {
+        console.log(e);
+        if (e.key === 'Enter') {
+            doSearch();
+        }
     }
-  }, [location.state.keyword])
-
-  // useEffect(() => {
-  //   doSearch();
-  // }, [keyword])
-
-  
-
-  return (
-    <div className='w-full px-48 mt-16'>
-      <div className={'flex justify-between items-center mb-16'}>
-        <p className={'text-3xl text-accent font-bold'}>검색 결과</p>
-        <div className="form-control" style={{ position: 'relative', paddingRight: '0px' }}>
-          <input onKeyPress={handleOnKeyPress} value={keyword} onChange={(e) => setKeyword(e.target.value)} type="text" placeholder="검색어를 입력해주세요" className={"input w-60 text-black text-xs rounded-xl input-sm bg-secondary"} />
-          <button onClick={() => doSearch()} className={'btn btn-xs btn-secondary border-none btn-circle'}  style={{ position: 'absolute', top:'4px', right:'10px', height:'1rem'}}>
-            <SearchIcon style={{height:'1rem'}}/>
-          </button>
-        </div>
-      </div>
-
-
-      <div className={'mb-20'}>
-        <div className={'flex mb-3'}>
-          <ListTitle className={'text-xl'} titleText={'강사 목록'} onClickEvent={() => navigate('/searchTeacher')}/>
-        </div>
-        <div>
-          { studio.teachersSearched.map((teacher, idx) => 
-              idx < 3 ? <SearchTeacherListItem key={teacher.userId} teacherInfo={teacher}/> : null
-          )}
-        </div>
-      </div>
-
-
-      <div className={'mb-20'}>
-        <div className={'flex mb-5'}>
-          <ListTitle className={'text-xl'} titleText={'강의 목록'} onClickEvent={() => navigate('/searchLecture')}/>
-        </div>
-        <div className={'flex justify-evenly'}>
-          { lecture.lecturesSearched.map((lecture, idx) => 
-              idx < 3 ? <LectureItemCard key={lecture.lectureId} thisLecture={lecture}/> : null
-          )}
-        </div>
-      </div>
-    </div>
-  )
+    
+    return (
+        <>
+            <div className="flex w-full">
+                <div className="flex-auto px-40 pt-20">
+                    <div className="flex justify-between items-center">
+                        <p className="text-3xl font-bold text-accent mb-6 mr-3">강사 목록</p>
+                        <div className="flex items-center">
+                            <StudioLectureListCategorySelectBox />
+                        </div>
+                        <div className="form-control" style={{ position: 'relative', paddingRight: '0px' }}>
+                            <input onKeyPress={handleOnKeyPress} value={keyword} onChange={(e) => setKeyword(e.target.value)} type="text" placeholder="검색어를 입력해주세요" className={"input w-60 text-black text-xs rounded-xl input-sm bg-secondary"} />
+                            <button onClick={() => doSearch()} className={'btn btn-xs btn-secondary border-none btn-circle'}  style={{ position: 'absolute', top:'4px', right:'10px', height:'1rem'}}>
+                                <SearchIcon style={{height:'1rem'}}/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex py-12 px-0">
+                        <div className="flex flex-wrap justify-start gap-9">
+                            {lectures?.map((lecture) => (
+                                <LectureItemCard key={lecture.lectureId} thisLecture={lecture}/>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default MainSearchPage
+export default MainSearchTeacher;
