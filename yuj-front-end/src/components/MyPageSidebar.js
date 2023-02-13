dee4c61d2fb4865882d6a351c666601a86673cca
배진호 <qowlsgh99@naver.com>
2023-02-13 14:35:24 +0900

    Feat: "사이드바 프로필사진, 닉네임, 이메일 표시"
    마이페이지 사이드바도 이제  스튜디오 사이드바와같이 사진, 닉네임, 이메일을
    표시합니다.

--- yuj-front-end/src/components/MyPageSidebar.js
+++ yuj-front-end/src/components/MyPageSidebar.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Styles from './MyPageSidebar.module.css';
 
 
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, useNavigate } from 'react-router-dom';
 
 function MyPageSidebar() {
+
+  const navigate = useNavigate();
 
   // 사이드바 메뉴 추가하려면 아래 입력
   const sidebarMenu = [
@@ -25,14 +27,21 @@ function MyPageSidebar() {
   return (
 
     <div className="drawer-side">
-      <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-      <ul className={"flex items-start justify-between menu p-5  bg-primary text-base-content " + Styles.myPageSidebar}>
+      <ul className={"flex items-start justify-between menu p-5 bg-primary text-base-content " + Styles.myPageSidebar}>
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <div className={'cursor-pointer'} onClick={() => navigate('/mypage/dashboard')}>
+          <img className={Styles.myProfileImg} src="/assets/Sample3.jpg" />
+          {/* <img className='profile-img' src={studioDetail.profileImagePath}/> */}
+          <p className={Styles.myNickname + ' mt-6 justify-center'}>닉네임</p>
+          <p className={Styles.myEmail + ' mt-3 justify-center	'}>yuj@gmail.com</p>
+        </div>
+
         <div>
           {/* 사이드바 내용물 map이용 출력 */}
           {sidebarMenu.map((menu, index) => {
             return (
               <li key={index}>
-                <Link to={menu.path} className={Styles.sidebarMenu}>
+                <Link to={menu.path} className={Styles.sidebarMenu + ' justify-center'}>
                   {menu.name}
                 </Link>
               </li>
