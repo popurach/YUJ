1a6e963c112dab8a78a8cc93ad5e8c4f03abb67b
배진호 <qowlsgh99@naver.com>
2023-02-14 15:49:43 +0900

    Feat: "마이페이지 대시보드 수강중, 수강완료 맞춰서 API 제작"

--- yuj-back-end/src/main/java/com/yuj/mypage/controller/MyPageController.java
+++ yuj-back-end/src/main/java/com/yuj/mypage/controller/MyPageController.java
@@ -1,6 +1,5 @@
 package com.yuj.mypage.controller;
 
-import com.yuj.lecture.domain.LectureSchedule;
 import com.yuj.mypage.dto.request.MyPageUserInfoRequestDTO;
 import com.yuj.mypage.dto.response.MyPageLectureScheduleResponseDTO;
 import com.yuj.mypage.dto.response.MyPageUserLectureResponseDTO;
@@ -21,25 +20,57 @@ public class MyPageController {
 
     private final MyPageService myPageService;
 
+    //userId기반 모든 강의 가져오기
     @GetMapping("/dashboard/{userId}")
     public ResponseEntity<?> getUserLecture(@PathVariable Long userId) {
-        List<MyPageUserLectureResponseDTO> userLecturesById = myPageService.getUserLecturesById(userId);
-        System.out.println("마이페이지 컨트롤러진입후 할당");
+        List<MyPageUserLectureResponseDTO> userLecturesById = myPageService.getAllUserLecturesById(userId);
+        System.out.println("마이페이지 userId기반 컨트롤러진입후 할당");
 
         if (userLecturesById != null) {
-            System.out.println("마이페이지 컨트롤러정상반환");
+            System.out.println("마이페이지userId기반  컨트롤러정상반환");
             return ResponseEntity.ok().body(userLecturesById);
         } else if (userLecturesById == null) {
-            System.out.println("마이페이지 컨트롤러널");
-            return ResponseEntity.ok().body("수강중인 강의가 없습니다.");
+            System.out.println("마이페이지userId기반  컨트롤러널");
+            return ResponseEntity.ok().body("수강한 강의가 없습니다.");
         }
         //오늘날짜이후로 가장 가까운 start시간 3개로해야함 일단은 전체 lecture라도 반환해보기
 
-        System.out.println("마이페이지 컨트롤러 뭔가 오류");
+        System.out.println("마이페이지 userId기반  컨트롤러 뭔가 오류");
         return ResponseEntity.badRequest().body("오류가 발생하였습니다.");
 
 //        return new ResponseEntity(userLecturesById, HttpStatus.OK);
     }
+
+    // 현재 수강중인 강의만 가져오기(진행중)
+    @GetMapping("/dashboard/currentlectures/{userId}")
+    public ResponseEntity<?> getCurrentLectures(@PathVariable Long userId) throws Exception {
+        List<MyPageUserLectureResponseDTO> userLecturesById = myPageService.getCurrentLecturesById(userId);
+        System.out.println("현재수강중강의 가져오기 컨트롤러 진입");
+        if (userLecturesById != null) {
+            System.out.println("현재수강중강의 가져오기 컨트롤러 성공");
+            return ResponseEntity.ok().body(userLecturesById);
+        } else if (userLecturesById == null) {
+            return ResponseEntity.ok().body("진행중인 강의가 없습니다.");
+        }
+        //오늘날짜이후로 가장 가까운 start시간 3개로해야함 일단은 전체 lecture라도 반환해보기
+        return ResponseEntity.badRequest().body("오류가 발생하였습니다.");
+    }
+
+    // 수강 완료된 강의만 가져오기
+    @GetMapping("/dashboard/completedlectures/{userId}")
+    public ResponseEntity<?> getCompletedLectures(@PathVariable Long userId) throws Exception {
+        List<MyPageUserLectureResponseDTO> userLecturesById = myPageService.getCompletedLecturesById(userId);
+        System.out.println("수강완료된강의 컨트롤러 진입");
+
+        if (userLecturesById != null) {
+            System.out.println("수강완료된강의 컨트롤러 리턴 성공");
+            return ResponseEntity.ok().body(userLecturesById);
+        } else if (userLecturesById == null) {
+            return ResponseEntity.ok().body("수강 완료한 강의가 없습니다.");
+        }
+        return ResponseEntity.badRequest().body("오류가 발생하였습니다.");
+    }
+
 
     @GetMapping("/dashboard/lectureSchedule/{lectureId}")
     public ResponseEntity<?> getLectureSchedule(@PathVariable Long lectureId) {
--- yuj-back-end/src/main/java/com/yuj/mypage/service/MyPageService.java
+++ yuj-back-end/src/main/java/com/yuj/mypage/service/MyPageService.java
@@ -4,6 +4,7 @@ import com.yuj.lecture.domain.Lecture;
 import com.yuj.lecture.domain.LectureSchedule;
 import com.yuj.lecture.domain.UserLecture;
 import com.yuj.lecture.domain.Yoga;
+import com.yuj.lecture.repository.LectureRepository;
 import com.yuj.lecture.repository.LectureScheduleRepository;
 
 import com.yuj.mypage.dto.request.MyPageUserInfoRequestDTO;
@@ -13,12 +14,14 @@ import com.yuj.mypage.repository.MyPageUserInfoRepository;
 import com.yuj.mypage.repository.MyPageUserLectureRepository;
 import com.yuj.user.domain.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -30,26 +33,68 @@ public class MyPageService {
 
     private final MyPageUserInfoRepository myPageUserInfoRepository;
 
-//    userId를 사용하여 수강하고있는 모든 강의정보 가져오기
-    public List<MyPageUserLectureResponseDTO> getUserLecturesById(long userId){
+    //    userId를 사용하여 수강하고있는 모든 강의정보 가져오기
+    public List<MyPageUserLectureResponseDTO> getAllUserLecturesById(long userId) {
         List<UserLecture> allByUserUserId = myPageUserLectureRepository.findAllByUser_UserId(userId);
 
-        List<MyPageUserLectureResponseDTO>myPageUserLectureResponseDTOS = new ArrayList<>();
+        List<MyPageUserLectureResponseDTO> myPageUserLectureResponseDTOS = new ArrayList<>();
 
-        for(UserLecture dto : allByUserUserId){
+        for (UserLecture dto : allByUserUserId) {
             myPageUserLectureResponseDTOS.add(entityToUserLectureResponseDTO(dto));
         }
 
         return myPageUserLectureResponseDTOS;
     }
 
-//    lectureId를 사용하여 모든 강의 스케쥴 가져오기
-    public List<MyPageLectureScheduleResponseDTO> getLectureScheduleByLectureId(long lectureId){
+    //    userId기반 모든 강의중에 현재 수강중인 강의만 가져오기
+    public List<MyPageUserLectureResponseDTO> getCurrentLecturesById(long userId) throws Exception {
+        //서버 시간 가져오기
+        URL url = new URL("https://i8a504.p.ssafy.io");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("HEAD");
+        Date serverDate = new Date(conn.getHeaderFieldDate("Date", 0)); //서버시간 serverDate
+        LocalDate localDate = serverDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        List<UserLecture> allByUserUserId = myPageUserLectureRepository.findAllByUser_UserId(userId);
+
+        List<MyPageUserLectureResponseDTO> myPageUserLectureResponseDTOS = new ArrayList<>();
+
+        for (UserLecture dto : allByUserUserId) {
+            if (dto.getLecture().getEndDate().isAfter(localDate)) {
+                myPageUserLectureResponseDTOS.add(entityToUserLectureResponseDTO(dto));
+            }
+        }
+        return myPageUserLectureResponseDTOS;
+    }
+
+    //    userId기반 모든 강의중에 완료된 강의만 가져오기
+    public List<MyPageUserLectureResponseDTO> getCompletedLecturesById(long userId) throws Exception {
+        //서버 시간 가져오기
+        URL url = new URL("https://i8a504.p.ssafy.io");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("HEAD");
+        Date serverDate = new Date(conn.getHeaderFieldDate("Date", 0)); //서버시간 serverDate
+        LocalDate localDate = serverDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        List<UserLecture> allByUserUserId = myPageUserLectureRepository.findAllByUser_UserId(userId);
+
+        List<MyPageUserLectureResponseDTO> myPageUserLectureResponseDTOS = new ArrayList<>();
+
+        for (UserLecture dto : allByUserUserId) {
+            if (dto.getLecture().getEndDate().isBefore(localDate)) {
+                myPageUserLectureResponseDTOS.add(entityToUserLectureResponseDTO(dto));
+            }
+        }
+        return myPageUserLectureResponseDTOS;
+    }
+
+    //    lectureId를 사용하여 모든 강의 스케쥴 가져오기
+    public List<MyPageLectureScheduleResponseDTO> getLectureScheduleByLectureId(long lectureId) {
         List<LectureSchedule> allByLectureLectureId = lectureScheduleRepository.findAllByLecture_LectureId(lectureId);
 
         List<MyPageLectureScheduleResponseDTO> myPageLectureScheduleResponseDTOS = new ArrayList<>();
 
-        for(LectureSchedule dto : allByLectureLectureId){
+        for (LectureSchedule dto : allByLectureLectureId) {
             myPageLectureScheduleResponseDTOS.add(entityToLectureScheduleResponseDTO(dto));
         }
 
@@ -58,23 +103,23 @@ public class MyPageService {
 
 //    유저 정보 수정
 
-    public Optional<User> updateUser(Long userId, MyPageUserInfoRequestDTO myPageUserInfoRequestDTO){
+    public Optional<User> updateUser(Long userId, MyPageUserInfoRequestDTO myPageUserInfoRequestDTO) {
         Optional<User> user = this.myPageUserInfoRepository.findById(userId);
 
-        user.ifPresent(u ->{
-            if(myPageUserInfoRequestDTO.getProfileImage() != null) {
+        user.ifPresent(u -> {
+            if (myPageUserInfoRequestDTO.getProfileImage() != null) {
                 u.setProfileImagePath(myPageUserInfoRequestDTO.getProfileImage());
             }
-            if(myPageUserInfoRequestDTO.getNickname() != null){
+            if (myPageUserInfoRequestDTO.getNickname() != null) {
                 u.setNickname(myPageUserInfoRequestDTO.getNickname());
             }
-            if(myPageUserInfoRequestDTO.getPassword() != null){
+            if (myPageUserInfoRequestDTO.getPassword() != null) {
                 u.setPassword(myPageUserInfoRequestDTO.getPassword());
             }
-            if(myPageUserInfoRequestDTO.getPhone() != null){
+            if (myPageUserInfoRequestDTO.getPhone() != null) {
                 u.setPhone(myPageUserInfoRequestDTO.getPhone());
             }
-            if(myPageUserInfoRequestDTO.getEmail() != null){
+            if (myPageUserInfoRequestDTO.getEmail() != null) {
                 u.setEmail(myPageUserInfoRequestDTO.getEmail());
             }
             this.myPageUserInfoRepository.save(u);
@@ -96,7 +141,7 @@ public class MyPageService {
 //        return userRepository.save(user);
 //    }
 
-    private MyPageUserLectureResponseDTO entityToUserLectureResponseDTO(UserLecture userLecture){
+    private MyPageUserLectureResponseDTO entityToUserLectureResponseDTO(UserLecture userLecture) {
 
         Lecture lecture = userLecture.getLecture();
         User user = lecture.getUser();
@@ -124,7 +169,7 @@ public class MyPageService {
                 build();
     }
 
-    private MyPageLectureScheduleResponseDTO entityToLectureScheduleResponseDTO(LectureSchedule lectureSchedule){
+    private MyPageLectureScheduleResponseDTO entityToLectureScheduleResponseDTO(LectureSchedule lectureSchedule) {
         Lecture lecture = lectureSchedule.getLecture();
 
         return MyPageLectureScheduleResponseDTO.builder().
