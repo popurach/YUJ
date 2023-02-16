48ae14e0353f9333ef207d8eb4412484650b99f4
ohhyunkyu <uyk0928@gmail.com>
2023-02-16 18:05:26 +0900

    Feat: 수강생 수강 신청, 취소, 재수강

--- yuj-back-end/src/main/java/com/yuj/config/security/SecurityConfiguration.java
+++ yuj-back-end/src/main/java/com/yuj/config/security/SecurityConfiguration.java
@@ -45,6 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/users").permitAll() 
                 .antMatchers(HttpMethod.POST, "/users").permitAll()             //   회원가입은 토큰 없이 가능
                 .antMatchers(HttpMethod.GET, "/lectures").permitAll()
+                .antMatchers(HttpMethod.PUT, "/lectures/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/reissue").permitAll()            //   회원가입은 토큰 없이 가능
                 .antMatchers(HttpMethod.POST, "/openvidu/**").permitAll()          //  토큰 재발행은 토큰 없이 가능
                 .antMatchers(HttpMethod.GET, "/mypage/**").permitAll()          //테스트용 마이페이지 임시허용(추후삭제)
