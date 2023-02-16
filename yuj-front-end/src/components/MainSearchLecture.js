63ebe7707f6b397dbdd3b4effed1fbf665934759
허재성 <cork2586@naver.com>
2023-02-16 22:10:16 +0900

    Fix: MainSearchLecture.js 불필요한 </div> 태그 제거

--- yuj-front-end/src/components/MainSearchLecture.js
+++ yuj-front-end/src/components/MainSearchLecture.js
@@ -27,34 +27,59 @@ const MainSearchTeacher = () => {
     }
   }, []);
 
-    return (
-        <>
-            <div className="flex w-full">
-                <div className="flex-auto px-40 pt-20">
-                    <div className="flex justify-between items-center">
-                        <p className="text-3xl font-bold text-accent mr-3">강의 목록</p>
-                        <div className="flex">
-                            <div className="flex items-center">
-                                <MainSearchLectureListCategorySelectBox keyword={ keyword} />
-                            </div>
-                            <div className="form-control" style={{ position: 'relative', paddingRight: '0px' }}>
-                                <input onKeyPress={handleOnKeyPress} value={keyword} onChange={(e) => setKeyword(e.target.value)} type="text" placeholder="검색어를 입력해주세요" className={"input w-60 text-black text-xs rounded-xl input-sm bg-secondary"} />
-                                <button onClick={() => doSearch()} className={'btn btn-xs btn-secondary border-none btn-circle'}  style={{ position: 'absolute', top:'4px', right:'10px', height:'1rem'}}>
-                                    <SearchIcon style={{height:'1rem'}}/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex py-12 px-0">
-                        <div className="flex flex-wrap justify-start gap-9">
-                            {lectures?.map((lecture) => (
-                                console.log("lecture는"),
-                                console.log(lecture),
-                                <LectureItemCard key={lecture.lectureId} thisLecture={lecture}/>
-                            ))}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="flex w-full">
+        <div className="flex-auto px-40 pt-20">
+          <div className="flex justify-between items-center">
+            <p className="text-3xl font-bold text-accent mr-3">강의 목록</p>
+            <div className="flex">
+              <div className="flex items-center">
+                <MainSearchLectureListCategorySelectBox keyword={keyword} />
+              </div>
+              <div
+                className="form-control"
+                style={{ position: "relative", paddingRight: "0px" }}
+              >
+                <input
+                  onKeyPress={handleOnKeyPress}
+                  value={keyword}
+                  onChange={(e) => setKeyword(e.target.value)}
+                  type="text"
+                  placeholder="검색어를 입력해주세요"
+                  className={
+                    "input w-60 text-black text-xs rounded-xl input-sm bg-secondary"
+                  }
+                />
+                <button
+                  onClick={() => doSearch()}
+                  className={"btn btn-xs btn-secondary border-none btn-circle"}
+                  style={{
+                    position: "absolute",
+                    top: "4px",
+                    right: "10px",
+                    height: "1rem",
+                  }}
+                >
+                  <SearchIcon style={{ height: "1rem" }} />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex py-12 px-0">
+            <div className="flex flex-wrap justify-start gap-9">
+              {lectures?.map(
+                (lecture) => (
+                  console.log("lecture는"),
+                  console.log(lecture),
+                  (
+                    <LectureItemCard
+                      key={lecture.lectureId}
+                      thisLecture={lecture}
+                    />
+                  )
+                )
+              )}
             </div>
           </div>
         </div>
