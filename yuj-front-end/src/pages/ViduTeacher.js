a529ac70dd9548893de4a3feb3766d857fb2d53b
ohhyunkyu <uyk0928@gmail.com>
2023-02-20 17:52:54 +0900

    Feat: 강의 진행 중 강사 화면 구성 조정

--- yuj-front-end/src/pages/ViduTeacher.js
+++ yuj-front-end/src/pages/ViduTeacher.js
@@ -466,9 +466,8 @@ class Vidu extends Component {
         }
 
         const VideoContainer = styled.div`
-            background: white;
-            /* display: flex !important; */
-            /* flex-wrap: wrap !important; */
+           
+            background: linear-gradient(to top left, #F8F6F3, #B7AFBD);
             justify-content: center;
             top:0px;
             width: 100vw;
@@ -487,7 +486,7 @@ class Vidu extends Component {
             width: 100%;
             height: 5vh;
             position: fixed;
-            bottom: 0px;
+            bottom: 1%;
 
             display: flex !important;
             clear: both;
@@ -544,18 +543,18 @@ class Vidu extends Component {
                             <ButtonContainer>
                                 <img className='yuj-logo' alt='No Image' src='/assets/YujMainLogo.svg' style={{ marginBottom: '10px' }}></img>
                                 <button className="clickControl " onClick={this.videoControl}><div className="flex w-full justify-center">{this.state.publisher.properties.publishVideo === true ?
-                                    <span className="material-symbols-outlined">videocam</span> : <span className="material-symbols-outlined">videocam_off</span>}    {this.state.videoMessage}</div>
+                                    <span className="material-symbols-outlined">videocam</span> : <span className="material-symbols-outlined">videocam_off</span>}  &nbsp;&nbsp;  {this.state.videoMessage}</div>
                                 </button>
                                 <button className="clickControl" onClick={this.voiceControl}><div className="flex w-full justify-center">{this.state.publisher.properties.publishAudio === true ?
-                                    <span className="material-symbols-outlined">mic</span> : <span className="material-symbols-outlined">mic_off</span>}    {this.state.voiceMessage}</div>
+                                    <span className="material-symbols-outlined">mic</span> : <span className="material-symbols-outlined">mic_off</span>} &nbsp;&nbsp; {this.state.voiceMessage}</div>
                                 </button>
                                 <button className="clickControl" onClick={this.listControl}><div className="flex w-full justify-center">{this.state.liston === true ?
-                                    <span className="material-symbols-outlined">person</span> : <span className="material-symbols-outlined">person_off</span>} {this.state.listMessage}</div>
+                                    <span className="material-symbols-outlined">person</span> : <span className="material-symbols-outlined">person_off</span>} &nbsp;&nbsp; {this.state.listMessage}</div>
                                 </button>
                                 <button className="clickControl" onClick={this.chattoggle}><div className="flex w-full justify-center">{this.state.chaton === true ?
-                                    <span className="material-symbols-outlined">chat</span> : <span className="material-symbols-outlined">speaker_notes_off</span>} {this.state.chatMessage}</div>
+                                    <span className="material-symbols-outlined">chat</span> : <span className="material-symbols-outlined">speaker_notes_off</span>} &nbsp;&nbsp; {this.state.chatMessage}</div>
                                 </button>
-                                <button className="clickControl" onClick={this.leaveSession}><div className="flex w-full justify-center"><span className="material-symbols-outlined">exit_to_app</span> 종료</div></button>
+                                <button className="clickControl" onClick={this.leaveSession}><div className="flex w-full justify-center"><span className="material-symbols-outlined">exit_to_app</span> &nbsp;&nbsp; 종료</div></button>
                             </ButtonContainer>
                         ) : null}
                     </div>
