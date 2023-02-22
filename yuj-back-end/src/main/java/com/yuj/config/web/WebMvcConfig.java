cf9744a1d3c782629406c94ac6b89b500f910aa6
김인중 <qwe_asd13579@naver.com>
2023-02-22 12:58:24 +0900

    Fix: "cors 헤더 허용"

--- yuj-back-end/src/main/java/com/yuj/config/web/WebMvcConfig.java
+++ yuj-back-end/src/main/java/com/yuj/config/web/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.yuj.config.web;
 
+import io.swagger.models.HttpMethod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,6 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer  {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                .allowedHeaders("*")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 }
